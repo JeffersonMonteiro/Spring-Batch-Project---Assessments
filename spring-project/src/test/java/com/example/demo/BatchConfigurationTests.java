cdf131563da890a1260069a98a0dfff4db5a5128
Joao Vitor Figueiredo <joaovitor.98@hotmail.com>
2020-02-14 14:58:47 -0300

    Fixed test dependencies

--- spring-project/src/test/java/com/example/demo/BatchConfigurationTests.java
+++ spring-project/src/test/java/com/example/demo/BatchConfigurationTests.java
@@ -1,8 +1,9 @@
 package com.example.demo;
 
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.test.JobLauncherTestUtils;
@@ -10,11 +11,11 @@ import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.batch.runtime.BatchStatus;
-
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @SpringBatchTest
 public class BatchConfigurationTests {
 
@@ -29,7 +30,7 @@ public class BatchConfigurationTests {
         //jobLauncherTestUtils = new JobLauncherTestUtils();
 
         JobExecution jobExecution = jobLauncherTestUtils.launchStep("step1");
-        Assert.assertEquals(jobExecution.getStatus(), BatchStatus.COMPLETED);
+        Assertions.assertEquals(jobExecution.getStatus(), BatchStatus.COMPLETED);
 
 
     }
