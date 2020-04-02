c114511df9890b75f525ecfe0de4e29122a93ede
Maryanna Morais (Retail) <ma40044405@wipro.com>
2020-04-02 11:47:33 -0300

    add changes in test classes

--- spring-boot-rest/src/test/java/com/example/demo/integration/ProductControllerIntegrationTest.java
+++ spring-boot-rest/src/test/java/com/example/demo/integration/ProductControllerIntegrationTest.java
@@ -1,6 +1,7 @@
 package com.example.demo.integration;
 
 import com.example.demo.controller.ProductController;
+import com.example.demo.controller.UserController;
 import com.example.demo.model.Product;
 import com.example.demo.model.User;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -8,23 +9,30 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 public class ProductControllerIntegrationTest {
 
     MockMvc mockMvc;
+    MockMvc mockMvcUser;
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -32,24 +40,42 @@ public class ProductControllerIntegrationTest {
     @Autowired
     private ProductController productController;
 
+    @Autowired
+    private UserController userController;
+
     @Before
     public void setup() {
+        this.mockMvcUser = standaloneSetup(this.userController).build();
         this.mockMvc = standaloneSetup(this.productController).build();
     }
 
     @Test
-    public void AddProduct_OK() throws Exception {
+    public void GivenAUserAndAProduct_WhenSaveThisUserAndProduct_ThenShouldReturnOk() throws Exception {
+        User user = new User("Ana", "address", 22);
+        mockMvcUser.perform(post("/User/add")
+                .content(objectMapper.writeValueAsString(user))
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
         Product product = new Product("Dress");
-        mockMvc.perform(post("/User/{idUser}/Product/add", 4)
+        mockMvc.perform(post("/User/1/Product/add")
                 .content(objectMapper.writeValueAsString(product))
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
     }
 
     @Test
-    public void DeleteProduct_OK() throws Exception {
+    public void GivenAProductSaved_WhenDeleteThisProduct_ThenShouldReturnOk() throws Exception {
+        User user = new User("Ana", "address", 22);
+        mockMvcUser.perform(post("/User/add")
+                .content(objectMapper.writeValueAsString(user))
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
         Product product = new Product("Dress");
-        mockMvc.perform(delete("/User/4/Product/delete/5")
+        mockMvc.perform(post("/User/1/Product/add")
+                .content(objectMapper.writeValueAsString(product))
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+        mockMvc.perform(delete("/User/1/Product/delete/1")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(product)))
                 .andExpect(status().isOk());
--- spring-boot-rest/src/test/java/com/example/demo/integration/UserControllerIntegrationTest.java
+++ spring-boot-rest/src/test/java/com/example/demo/integration/UserControllerIntegrationTest.java
@@ -7,9 +7,13 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.hamcrest.Matchers.is;
@@ -20,6 +24,8 @@ import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
+@AutoConfigureTestDatabase(replace = Replace.ANY)
 public class UserControllerIntegrationTest {
 
     MockMvc mockMvc;
@@ -36,55 +42,36 @@ public class UserControllerIntegrationTest {
     }
 
     @Test
-    public void getUsersTest() throws Exception {
+    public void WhenTheMethodGet_ThenShouldReturnStatusOk() throws Exception {
         mockMvc.perform(get("/User/get").contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
     }
 
     @Test
-    public void findAllUsers_OK() throws Exception {
+    public void GivenAUser_WhenISave_TheMethodGetAllUser_ThenShouldReturnTheSameUser() throws Exception {
+        User user = new User("Ana", "address", 22);
+        mockMvc.perform(post("/User/add")
+                .content(objectMapper.writeValueAsString(user))
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
         mockMvc.perform(get("/User/get"))
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(1)))
                 .andExpect(jsonPath("$[0].name", is("Ana")))
                 .andExpect(jsonPath("$[0].address", is("address")))
-                .andExpect(jsonPath("$[0].age", is(22)));
+                .andExpect(jsonPath("$[0].age", is(22)))
+                .andExpect(jsonPath("$[0].idUser", is(1)));
     }
 
     @Test
-    public void addUser() throws Exception {
+    public void GivenAUser_WhenSave_ThenReturnTheSameUserWithId1() throws Exception {
         User user = new User("Ana", "address", 22);
         mockMvc.perform(post("/User/add")
                 .content(objectMapper.writeValueAsString(user))
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.name", is("Ana")))
-                .andExpect(jsonPath("$.address", is("address")))
-                .andExpect(jsonPath("$.age", is(22)));
-    }
-
-    @Test
-    public void deleteUser_OK() throws Exception {
-        User user = new User(1L, "Ana", "address", 22);
-        mockMvc.perform(delete("/User/delete/{id}", 4).contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(user)))
                 .andExpect(status().isOk());
-    }
-
-    //Do changes
-    @Test
-    public void updateUser_OK() throws Exception {
-        User userUpdate = new User("Ana", "Address", 22);
-        mockMvc.perform(put("/User/{id}", 4)
-                .content(objectMapper.writeValueAsString(userUpdate))
-                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    public void getUserById_OK() throws Exception {
-        mockMvc.perform(get("/User/get/{id}", 4))
+        mockMvc.perform(get("/User/get/{id}", 1))
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.name", is("Ana")))
@@ -93,7 +80,39 @@ public class UserControllerIntegrationTest {
     }
 
     @Test
-    public void getUsersByName_OK() throws Exception {
+    public void GivenAUserSaved_WhenDeleteThisUser_ThenShouldReturnOk() throws Exception {
+        User user = new User(1L, "Ana", "address", 22);
+        mockMvc.perform(post("/User/add")
+                .content(objectMapper.writeValueAsString(user))
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+        mockMvc.perform(delete("/User/delete/{id}", 1).contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(user)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void GivenAUser_WhenIGetById_ThenShouldReturnTheSameID() throws Exception {
+        User user = new User("Ana", "address", 22);
+        mockMvc.perform(post("/User/add")
+                .content(objectMapper.writeValueAsString(user))
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+        mockMvc.perform(get("/User/get/{id}", 1))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is("Ana")))
+                .andExpect(jsonPath("$.address", is("address")))
+                .andExpect(jsonPath("$.age", is(22)));
+    }
+
+    @Test
+    public void GivenAName_WhenIFindByName_ThenShouldReturnAListWhitTheUser() throws Exception {
+        User user = new User("Ana", "address", 22);
+        mockMvc.perform(post("/User/add")
+                .content(objectMapper.writeValueAsString(user))
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
         mockMvc.perform(get("/User/name/{name}", "Ana"))
                 .andExpect(jsonPath("$", hasSize(1)))
                 .andExpect(jsonPath("$[0].name", is("Ana")))
